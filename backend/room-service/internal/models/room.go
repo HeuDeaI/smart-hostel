4ad559832d2dc57a2bd5c32a0f914b6166ce46e4
Tsimka <p.profitpt@gmail.com>
2025-05-16 18:23:36 +0300

    add bookin structure

--- backend/room-service/internal/models/room.go
+++ backend/room-service/internal/models/room.go
@@ -1,6 +1,8 @@
 package models
 
 type Room struct {
-	ID     uint `gorm:"primaryKey" json:"id"`
-	Number int  `gorm:"unique;not null" json:"number"`
+	ID       uint      `gorm:"primaryKey" json:"id"`
+	Number   int       `gorm:"unique;not null" json:"number"`
+	IsBooked bool      `gorm:"default:false" json:"is_booked"`
+	Bookings []Booking `gorm:"foreignKey:RoomID"`
 }
