8e003600b8a455ac988de5b00af8e2ee68b105de
Vladislav <vladislav.malchevskiy.work@gmail.com>
2025-05-16 12:20:21 +0300

    4-commit

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -7,23 +7,23 @@ export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(false);
-  const [userToken, setUserToken] = useState(null);
-  const [userInfo, setUserInfo] = useState(null);
-  //   const [userToken, setUserToken] = useState("test_token");
-  //   const [userInfo, setUserInfo] = useState({
-  //     _id: "test-id",
-  //     full_name: "Test User",
-  //     email: "test@example.com",
-  //     member_type: "admin",
-  //     password: "12345", // Пароль (обязательное)
-  //     batch: "1", // Группа/курс
-  //     faculty: "rfict", // Факультет
-  //     member_type: "admin", // Тип пользователя (обязательное, по умолчанию "student")
-  //     mobile_no: "8033434343", // Номер телефона (обязательное)
-  //     registration_no: "test", // Регистрационный номер
-  //     gender: "male", // Пол (обязательное)
-  //     timestamps: true,
-  //   });
+  // const [userToken, setUserToken] = useState(null);
+  // const [userInfo, setUserInfo] = useState(null);
+  const [userToken, setUserToken] = useState("test_token");
+  const [userInfo, setUserInfo] = useState({
+    _id: "test-id",
+    full_name: "Test User",
+    email: "test@example.com",
+    member_type: "admin",
+    password: "12345", // Пароль (обязательное)
+    batch: "1", // Группа/курс
+    faculty: "rfict", // Факультет
+    member_type: "student", // Тип пользователя (обязательное, по умолчанию "student")
+    mobile_no: "8033434343", // Номер телефона (обязательное)
+    registration_no: "test", // Регистрационный номер
+    gender: "male", // Пол (обязательное)
+    timestamps: true,
+  });
   const [err, setErr] = useState(null);
 
   const isLoggedIn = async () => {
--- frontend/src/navigation/user/UserDrawer.js
+++ frontend/src/navigation/user/UserDrawer.js
@@ -7,7 +7,6 @@ import Dashboard from "../../screens/app/user/Dashboard";
 import Rooms from "../../screens/app/user/Rooms";
 import Announcements from "../../screens/app/user/Announcements";
 import Complains from "../../screens/app/user/Complains";
-import LatePasses from "../../screens/app/user/LatePasses";
 import Settings from "../../screens/app/user/Settings";
 import Notifications from "../../screens/app/user/Notifications";
 import PaymentReceipts from "../../screens/app/user/PaymentReceipts";
@@ -23,8 +22,6 @@ import ViewPaymentReceipt from "../../screens/app/user/payment_receipts/ViewPaym
 import AddPaymentReceipt from "../../screens/app/user/payment_receipts/AddPaymentReceipt";
 import AddNewComplain from "../../screens/app/user/complains/AddNewComplain";
 import ViewComplain from "../../screens/app/user/complains/ViewComplain";
-import RequestLatePass from "../../screens/app/user/late_passes/RequestLatePass";
-import ViewLatePass from "../../screens/app/user/late_passes/ViewLatePass";
 import ViewAnnouncement from "../../screens/app/user/announcements/ViewAnnouncement";
 
 import RoomDetails from "../../screens/app/admin/rooms/RoomDetails";
@@ -35,221 +32,179 @@ const DashboardStack = createStackNavigator();
 const RoomsStack = createStackNavigator();
 const AnnouncementStack = createStackNavigator();
 const ComplainsStack = createStackNavigator();
-const LatePassesStack = createStackNavigator();
 const HostelAdmininstrationStack = createStackNavigator();
 const SettingsStack = createStackNavigator();
 const NotificationsStack = createStackNavigator();
 
 export const UserDashboard = () => {
-   return (
-      <DashboardStack.Navigator
-         initialRouteName="UserHome"
-         screenOptions={{
-            headerShown: false,
-         }}
-      >
-         <DashboardStack.Screen name="UserHome" component={Dashboard} />
-         <DashboardStack.Screen
-            name="UserRoomsDashboard"
-            component={UserRooms}
-         />
-         <DashboardStack.Screen
-            name="UserRoomsAcceptanceDashboard"
-            component={RoomAcceptance}
-         />
-         <DashboardStack.Screen
-            name="UserPaymentReceiptsDashboard"
-            component={PaymentReceipts}
-         />
-         <DashboardStack.Screen
-            name="UserAddPaymentReceipts"
-            component={AddPaymentReceipt}
-         />
-         <DashboardStack.Screen
-            name="UserViewPaymentReceipts"
-            component={ViewPaymentReceipt}
-         />
-         <DashboardStack.Screen
-            name="UserComplainsDashboard"
-            component={UserComplains}
-         />
-         <DashboardStack.Screen
-            name="UserAddNewComplain"
-            component={AddNewComplain}
-         />
-         <DashboardStack.Screen
-            name="UserViewComplain"
-            component={ViewComplain}
-         />
-         <DashboardStack.Screen
-            name="UserHostelRulesDashboard"
-            component={HostelRules}
-         />
-         <DashboardStack.Screen
-            name="UserLatePassesDashboard"
-            component={UserLatePasses}
-         />
-         <DashboardStack.Screen
-            name="UserRequestLatePass"
-            component={RequestLatePass}
-         />
-         <DashboardStack.Screen
-            name="UserViewLatePass"
-            component={ViewLatePass}
-         />
-         <DashboardStack.Screen
-            name="UserAnnouncementsDashboard"
-            component={UserAnnouncements}
-         />
-         <DashboardStack.Screen
-            name="UserViewAnnouncement"
-            component={ViewAnnouncement}
-         />
+  return (
+    <DashboardStack.Navigator
+      initialRouteName="UserHome"
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <DashboardStack.Screen name="UserHome" component={Dashboard} />
+      <DashboardStack.Screen name="UserRoomsDashboard" component={UserRooms} />
+      <DashboardStack.Screen
+        name="UserRoomsAcceptanceDashboard"
+        component={RoomAcceptance}
+      />
+      <DashboardStack.Screen
+        name="UserPaymentReceiptsDashboard"
+        component={PaymentReceipts}
+      />
+      <DashboardStack.Screen
+        name="UserAddPaymentReceipts"
+        component={AddPaymentReceipt}
+      />
+      <DashboardStack.Screen
+        name="UserViewPaymentReceipts"
+        component={ViewPaymentReceipt}
+      />
+      <DashboardStack.Screen
+        name="UserComplainsDashboard"
+        component={UserComplains}
+      />
+      <DashboardStack.Screen
+        name="UserAddNewComplain"
+        component={AddNewComplain}
+      />
+      <DashboardStack.Screen name="UserViewComplain" component={ViewComplain} />
+      <DashboardStack.Screen
+        name="UserHostelRulesDashboard"
+        component={HostelRules}
+      />
 
-         <DashboardStack.Screen
-            name="UserViewPaymentReceipt"
-            component={ViewPaymentReceipt}
-         />
-         <DashboardStack.Screen
-            name="UserNotifications"
-            component={UserNotifications}
-         />
-      </DashboardStack.Navigator>
-   );
+      <DashboardStack.Screen
+        name="UserAnnouncementsDashboard"
+        component={UserAnnouncements}
+      />
+      <DashboardStack.Screen
+        name="UserViewAnnouncement"
+        component={ViewAnnouncement}
+      />
+
+      <DashboardStack.Screen
+        name="UserViewPaymentReceipt"
+        component={ViewPaymentReceipt}
+      />
+      <DashboardStack.Screen
+        name="UserNotifications"
+        component={UserNotifications}
+      />
+    </DashboardStack.Navigator>
+  );
 };
 
 export const UserRooms = () => {
-   return (
-      <RoomsStack.Navigator
-         initialRouteName="UserRooms"
-         screenOptions={{
-            headerShown: false,
-         }}
-      >
-         <RoomsStack.Screen name="UserRooms" component={Rooms} />
-         <RoomsStack.Screen
-            name="UserRoomAcceptanceView"
-            component={ViewRoomAcceptance}
-         />
-         <RoomsStack.Screen
-            name="UserPendingRoomChecklist"
-            component={RoomChecklistDetails}
-         />
-      </RoomsStack.Navigator>
-   );
+  return (
+    <RoomsStack.Navigator
+      initialRouteName="UserRooms"
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <RoomsStack.Screen name="UserRooms" component={Rooms} />
+      <RoomsStack.Screen
+        name="UserRoomAcceptanceView"
+        component={ViewRoomAcceptance}
+      />
+      <RoomsStack.Screen
+        name="UserPendingRoomChecklist"
+        component={RoomChecklistDetails}
+      />
+    </RoomsStack.Navigator>
+  );
 };
 
 export const UserAnnouncements = () => {
-   return (
-      <AnnouncementStack.Navigator
-         initialRouteName="UserAnnouncements"
-         screenOptions={{
-            headerShown: false,
-         }}
-      >
-         <AnnouncementStack.Screen
-            name="UserAnnouncements"
-            component={Announcements}
-         />
-         <AnnouncementStack.Screen
-            name="UserAddAnnouncement"
-            component={AddAnnouncement}
-         />
-         <AnnouncementStack.Screen
-            name="UserViewAnnouncement"
-            component={ViewAnnouncement}
-         />
-      </AnnouncementStack.Navigator>
-   );
+  return (
+    <AnnouncementStack.Navigator
+      initialRouteName="UserAnnouncements"
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <AnnouncementStack.Screen
+        name="UserAnnouncements"
+        component={Announcements}
+      />
+      <AnnouncementStack.Screen
+        name="UserAddAnnouncement"
+        component={AddAnnouncement}
+      />
+      <AnnouncementStack.Screen
+        name="UserViewAnnouncement"
+        component={ViewAnnouncement}
+      />
+    </AnnouncementStack.Navigator>
+  );
 };
 
 export const UserComplains = () => {
-   return (
-      <ComplainsStack.Navigator
-         initialRouteName="UserComplains"
-         screenOptions={{
-            headerShown: false,
-         }}
-      >
-         <ComplainsStack.Screen name="UserComplains" component={Complains} />
-         <ComplainsStack.Screen
-            name="UserViewComplain"
-            component={ViewComplain}
-         />
-      </ComplainsStack.Navigator>
-   );
-};
-
-export const UserLatePasses = () => {
-   return (
-      <LatePassesStack.Navigator
-         initialRouteName="UserLatePasses"
-         screenOptions={{
-            headerShown: false,
-         }}
-      >
-         <LatePassesStack.Screen name="UserLatePasses" component={LatePasses} />
-         <LatePassesStack.Screen
-            name="UserViewLatePass"
-            component={ViewLatePass}
-         />
-      </LatePassesStack.Navigator>
-   );
+  return (
+    <ComplainsStack.Navigator
+      initialRouteName="UserComplains"
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <ComplainsStack.Screen name="UserComplains" component={Complains} />
+      <ComplainsStack.Screen name="UserViewComplain" component={ViewComplain} />
+    </ComplainsStack.Navigator>
+  );
 };
 
 export const UserHostelAdmininstration = () => {
-   return (
-      <HostelAdmininstrationStack.Navigator
-         initialRouteName="UserHostelAdmininstration"
-         screenOptions={{
-            headerShown: false,
-         }}
-      >
-         <HostelAdmininstrationStack.Screen
-            name="UserHostelAdmininstration"
-            component={HostelAdministration}
-         />
-         <HostelAdmininstrationStack.Screen
-            name="UserViewLatePass"
-            component={ViewLatePass}
-         />
-      </HostelAdmininstrationStack.Navigator>
-   );
+  return (
+    <HostelAdmininstrationStack.Navigator
+      initialRouteName="UserHostelAdmininstration"
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <HostelAdmininstrationStack.Screen
+        name="UserHostelAdmininstration"
+        component={HostelAdministration}
+      />
+    </HostelAdmininstrationStack.Navigator>
+  );
 };
 
 export const UserSettings = () => {
-   return (
-      <SettingsStack.Navigator
-         initialRouteName="UserSettings"
-         screenOptions={{
-            headerShown: false,
-         }}
-      >
-         <SettingsStack.Screen name="UserSettings" component={Settings} />
-         <SettingsStack.Screen
-            name="UserChangeProfileDetails"
-            component={ChangeProfileDetails}
-         />
-         <SettingsStack.Screen
-            name="UserChangePassword"
-            component={ChangePassword}
-         />
-      </SettingsStack.Navigator>
-   );
+  return (
+    <SettingsStack.Navigator
+      initialRouteName="UserSettings"
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <SettingsStack.Screen name="UserSettings" component={Settings} />
+      <SettingsStack.Screen
+        name="UserChangeProfileDetails"
+        component={ChangeProfileDetails}
+      />
+      <SettingsStack.Screen
+        name="UserChangePassword"
+        component={ChangePassword}
+      />
+    </SettingsStack.Navigator>
+  );
 };
 
 export const UserNotifications = () => {
-   return (
-      <NotificationsStack.Navigator
-         initialRouteName="UserNotificationsView"
-         screenOptions={{
-            headerShown: false,
-         }}
-      >
-         <NotificationsStack.Screen
-            name="UserNotificationsView"
-            component={Notifications}
-         />
-      </NotificationsStack.Navigator>
-   );
+  return (
+    <NotificationsStack.Navigator
+      initialRouteName="UserNotificationsView"
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <NotificationsStack.Screen
+        name="UserNotificationsView"
+        component={Notifications}
+      />
+    </NotificationsStack.Navigator>
+  );
 };
