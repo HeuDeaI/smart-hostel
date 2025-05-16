1aa828dfccbdf441caf95db0dacda7df29ac59aa
Tsimka <p.profitpt@gmail.com>
2025-05-16 10:58:38 +0300

    add crud operations for user entity

---
+++ backend/internal/repositories/database.go
@@ -0,0 +1,19 @@
+package repositories
+
+import (
+	"backend/internal/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func InitDB() (*gorm.DB, error) {
+	dsn := "host=localhost user=postgres password=postgres dbname=smart_hotel port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	db.AutoMigrate(&models.User{})
+
+	return db, nil
+}
